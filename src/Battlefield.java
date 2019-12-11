e4af69bb35619b457f9afd8adec0833b25a66dbb
AdrianSedlak <sed19033@spengergasse.at>
2019-12-11 10:09:50 +0100

    Editing of Battlefield

--- src/Battlefield.java
+++ src/Battlefield.java
@@ -1,18 +1,43 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Battlefield {
-    private ArrayList<Unit>Figthers;
-    private int food;
+    private static ArrayList<Unit>Fighters;
+    private static ArrayList<Integer>choices;
+    private static int food;
 
-    public void Battlefield(int eat, int fighters){
-        for (int i = 0; i < fighters; i++){
-            Unit unit = new Unit("Neutral");
-            Figthers.add(unit);
-        }
-        this.food = eat;
+    public static void main(String[] args) {
+        Battlefield(20, 10);
+        choices();
+        evaluation();
     }
 
-    public void battle(){
+    public static void Battlefield(int eat, int fighters){
+        for (int i = 0; i < fighters; i++){
+            Unit unit = new Unit("Neutral");
+            Fighters.add(unit);
+        }
+        food = eat;
+    }
 
+    public static void choices(){
+        Random rand = new Random();
+        for (int i = 0; i < Fighters.size(); i++){
+            Fighters.get(i).setChoice(rand.nextInt(food));
+        }
+    }
+
+    public static void evaluation(){
+        int count = 1;
+        for (int i = 0; i < choices.size(); i++)
+        {
+            for (int j = i; j < choices.size() - 1 ; j++)
+            {
+
+                if(choices.get(j) == choices.get(j+1))
+                    count = count + 1;
+            }
+            System.out.println(choices.get(i) + "occurse" + count);
+        }
     }
 }
--- src/Unit.java
+++ src/Unit.java
@@ -1,12 +1,20 @@
 public class Unit {
     private String property;
+    private int choice;
+    private int stomache;
 
     public Unit(String prop){
         this.property = prop;
+        this.choice = 0;
+        this.stomache = 0;
     }
 
     public void setProperty(String prop){
         this.property = prop;
+    }
+
+    public void setChoice(int choice){
+        this.choice = choice;
     }
 
     public String getProperty(){
